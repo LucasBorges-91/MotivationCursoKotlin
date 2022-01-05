9176e1ee292868ef9ff7975b79e239b55497f8f8
Lucas Borges <lucas.pborges@yahoo.com.br>
2022-01-05 20:42:47 -0300

    adicionando evento de click para mudar de activity

--- app/src/main/java/br/com/borges/lucas/motivationapp/SplashActivity.kt
+++ app/src/main/java/br/com/borges/lucas/motivationapp/SplashActivity.kt
@@ -1,13 +1,34 @@
 package br.com.borges.lucas.motivationapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import br.com.borges.lucas.motivationapp.databinding.ActivityMainBinding
+import br.com.borges.lucas.motivationapp.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivitySplashBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_splash)
-
+    binding = ActivitySplashBinding.inflate(layoutInflater)
+    setContentView(binding.root)
     supportActionBar?.hide()
+
+    binding.btSave.setOnClickListener{
+      handleSave()
+    }
+  }
+
+  private fun handleSave() {
+    val name = binding.etName.text.toString()
+    if ( name != "" ) {
+      startActivity( Intent( this, MainActivity::class.java ) )
+    } else {
+      Toast.makeText( this, "Informe o nome para seguir em frente", Toast.LENGTH_SHORT)
+        .show()
+    }
   }
 }
