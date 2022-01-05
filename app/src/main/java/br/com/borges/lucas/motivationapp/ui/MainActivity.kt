6f72a2e122e13557ecec5aad712d8c6531eff5eb
Lucas Borges <lucas.pborges@yahoo.com.br>
2022-01-05 20:59:13 -0300

    criando sharedPreferences

--- app/src/main/java/br/com/borges/lucas/motivationapp/ui/MainActivity.kt
+++ app/src/main/java/br/com/borges/lucas/motivationapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.borges.lucas.motivationapp
+package br.com.borges.lucas.motivationapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.borges.lucas.motivationapp.R
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/br/com/borges/lucas/motivationapp/ui/SplashActivity.kt
+++ app/src/main/java/br/com/borges/lucas/motivationapp/ui/SplashActivity.kt
@@ -1,4 +1,4 @@
-package br.com.borges.lucas.motivationapp
+package br.com.borges.lucas.motivationapp.ui
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -6,25 +6,31 @@ import android.os.Bundle
 import android.widget.Toast
 import br.com.borges.lucas.motivationapp.databinding.ActivityMainBinding
 import br.com.borges.lucas.motivationapp.databinding.ActivitySplashBinding
+import br.com.borges.lucas.motivationapp.infra.SecurityPreferences
 
 class SplashActivity : AppCompatActivity() {
 
   private lateinit var binding: ActivitySplashBinding
+  private lateinit var mSecurityPreferences: SecurityPreferences
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = ActivitySplashBinding.inflate(layoutInflater)
     setContentView(binding.root)
     supportActionBar?.hide()
+    mSecurityPreferences = SecurityPreferences( this )
 
     binding.btSave.setOnClickListener{
       handleSave()
     }
+
+
   }
 
   private fun handleSave() {
     val name = binding.etName.text.toString()
     if ( name != "" ) {
+      mSecurityPreferences.storeString( "name", name )
       startActivity( Intent( this, MainActivity::class.java ) )
     } else {
       Toast.makeText( this, "Informe o nome para seguir em frente", Toast.LENGTH_SHORT)
