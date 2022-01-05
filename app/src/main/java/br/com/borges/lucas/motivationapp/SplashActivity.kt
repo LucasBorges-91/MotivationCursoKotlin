2da5b4657f509fed6eec23c448fe5da3ee0d4f12
Lucas Borges <lucas.pborges@yahoo.com.br>
2022-01-05 20:29:18 -0300

    criando splashLayout

---
+++ app/src/main/java/br/com/borges/lucas/motivationapp/SplashActivity.kt
@@ -0,0 +1,13 @@
+package br.com.borges.lucas.motivationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_splash)
+
+    supportActionBar?.hide()
+  }
+}
