6b2de222fa322276741fc284c43449265ebb6aaf
Lucas Borges <lucas.pborges@yahoo.com.br>
2022-01-05 22:05:10 -0300

    adicionando eventos de click na mainLayout

--- app/src/main/java/br/com/borges/lucas/motivationapp/ui/MainActivity.kt
+++ app/src/main/java/br/com/borges/lucas/motivationapp/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.borges.lucas.motivationapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import br.com.borges.lucas.motivationapp.R
 import br.com.borges.lucas.motivationapp.databinding.ActivityMainBinding
 import br.com.borges.lucas.motivationapp.infra.MotivationConstants
@@ -19,5 +20,43 @@ class MainActivity : AppCompatActivity() {
     supportActionBar?.hide()
     mSecurityPreferences = SecurityPreferences( this )
     binding.tvName.text = mSecurityPreferences.getString(MotivationConstants.KEY.PERSON_NAME)
+
+    binding.ivAll.setOnClickListener{
+      handleFilter( 1 )
+    }
+
+    binding.ivHappy.setOnClickListener{
+      handleFilter( 2 )
+    }
+
+    binding.ivMorning.setOnClickListener{
+      handleFilter( 3 )
+    }
+
+    binding.btNewPhrase.setOnClickListener{
+
+    }
+  }
+
+  private fun handleNewPhrase() {
+
+  }
+
+  private fun handleFilter( id: Int ) {
+    binding.ivAll.setColorFilter( ContextCompat.getColor( this, R.color.white ) )
+    binding.ivHappy.setColorFilter( ContextCompat.getColor( this, R.color.white ) )
+    binding.ivMorning.setColorFilter( ContextCompat.getColor( this, R.color.white ) )
+
+    when ( id ) {
+      1 -> {
+        binding.ivAll.setColorFilter( ContextCompat.getColor( this, R.color.colorAccent ) )
+      }
+      2 -> {
+        binding.ivHappy.setColorFilter( ContextCompat.getColor( this, R.color.colorAccent ) )
+      }
+      3 -> {
+        binding.ivMorning.setColorFilter( ContextCompat.getColor( this, R.color.colorAccent ) )
+      }
+    }
   }
 }
